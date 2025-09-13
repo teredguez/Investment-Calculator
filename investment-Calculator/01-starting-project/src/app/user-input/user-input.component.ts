70b8642fc7087c0c27691084b59bf939392a13ee
tere <teresadominguezotero@gmail.com>
2025-09-13 21:08:03 +0000

    calculate investment method added

--- investment-Calculator/01-starting-project/src/app/user-input/user-input.component.ts
+++ investment-Calculator/01-starting-project/src/app/user-input/user-input.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import type { InvestmentInput } from '../investment-input';
 
 @Component({
   selector: 'app-user-input',
@@ -9,14 +10,19 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  @Output() calculate = new EventEmitter<InvestmentInput>();
+
   enteredInitiaInvestment = '0';
   enteredAnnualInvestment = '0';
   enteredExpectedReturn = '5';
   enteredDuration = '10';
 
   onSubmit(){
-    console.log('Submitted');
-    console.log(this.enteredAnnualInvestment);
-    console.log(this.enteredDuration)
+    this.calculate.emit({
+    initialInvestment: +this.enteredInitiaInvestment, 
+    duration:+this.enteredDuration,
+    expectedReturn:+this.enteredExpectedReturn,
+    annualInvestment: +this.enteredInitiaInvestment
+    });
   }
 }
